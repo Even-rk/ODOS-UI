1671606988964486ddba86ec320a96a2d46877b2
Aeary <2921401460@qq.com>
2024-02-05 10:55:05 +0800

    update -V

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+declare module 'prismjs'
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '*.md' {
+  import { ComponentOptions } from 'vue'
+  const Component: ComponentOptions
+  export default Component
+}
