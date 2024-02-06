4ef028cd6c64fa0a8ad0c9ecdf6f042cd2507f88
Aeary <2921401460@qq.com>
2024-02-06 10:54:25 +0800

    icon + home update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>home</div>
+  <div class="odos-home">
+    <div class="banner" text="center">
+      <div class="banner-desc">
+        <h1>ODOS-UI</h1>
+        <p>ODOSUI,一套为医疗场景准备的vue3组件库</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.odos-home {
+  width: 800px;
+  margin: 20px auto;
+  padding: 40px 40px 0;
+  position: relative;
+  .banner-desc h1 {
+    font-size: 48px;
+    font-weight: 800;
+    margin-top: 40px;
+    margin: 0;
+    font-family: Inter, sans-serif;
+  }
+}
+[text~='center'] {
+  text-align: center;
+}
+</style>
