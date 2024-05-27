c99cd384566e9c630ccfbca4956522bbe9a6c4be
kwr011024 <kwr011024@163.com>
2024-05-27 17:24:16 +0800

    update:picker

--- packages/DatePicker/src/index.vue
+++ packages/DatePicker/src/index.vue
@@ -1,8 +1,33 @@
 <template>
-  <div class="odos-date-picker"></div>
+  <div class="odos-date-picker">
+    <odos-input v-model:value="datePicker" @blur="inputBlur" @focus="inputFocus" placeholder="请选择日期" />
+    <div
+      class="odos-date-picker-content"
+      v-if="isShowPicker"
+      :class="{
+        bottom: true,
+        top: false
+      }"
+    >
+      <div class="odos-date-picker-header"></div>
+      <div class="odos-date-picker-body"></div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const datePicker = ref()
+
+const isShowPicker = ref(false)
+const inputFocus = () => {
+  isShowPicker.value = true
+}
+const inputBlur = () => {
+  isShowPicker.value = false
+}
+</script>
 
 <style lang="scss">
 @import '/styles/datePicker.scss';
