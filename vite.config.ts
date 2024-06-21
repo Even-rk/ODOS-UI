a49f6167b2c6b1181b40bdd7ca849cf68b97fcdb
kwr011024 <kwr011024@163.com>
2024-06-21 14:06:22 +0800

    update:icon

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vuejsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
