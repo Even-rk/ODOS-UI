06f5c879cdbb9fa86adc59c7597940de4bf47779
xusheng <thinkpadcenter@163.com>
2025-03-04 08:53:48 +0800

    组件更新

--- packages/Button/index.ts
+++ packages/Button/index.ts
@@ -1,10 +1,19 @@
-import type { App, Plugin } from 'vue'
+// import type { App, Plugin } from 'vue'
+// import Button from './src/index.vue'
+
+// export const ButtonPlugin: Plugin = {
+//   install(app: App) {
+//     app.component('OdosButton', Button)
+//   }
+// }
+
+// export { Button }
+
+import { App } from 'vue'
 import Button from './src/index.vue'
 
-export const ButtonPlugin: Plugin = {
-  install(app: App) {
+export default {
+  install(app:App) {
     app.component('OdosButton', Button)
   }
 }
-
-export { Button }
