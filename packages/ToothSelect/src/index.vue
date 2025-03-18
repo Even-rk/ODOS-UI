c236f83d6f8042a4d8d2168d3da0216e0f3a4587
kwr011024 <kwr011024@163.com>
2025-03-18 11:15:30 +0800

    "version": "0.3.27": 修改牙位选择器

--- packages/ToothSelect/src/index.vue
+++ packages/ToothSelect/src/index.vue
@@ -3,7 +3,7 @@
     <div class="header">
       <div class="complete">
         <CheckBox v-model:value="toothVal" @change="toothValChange" mutex mutexOptionValue="All">
-          <CheckBoxItem :disabled="rangeLimit === 'tooth'" id="-1" value="-1" label="全口" />
+          <CheckBoxItem :disabled="props.rangeLimit === 'tooth'" id="-1" value="-1" label="全口" />
         </CheckBox>
       </div>
       <div class="info">
@@ -12,19 +12,19 @@
       </div>
     </div>
     <Tooth
-      :name="name"
+      :name="props.name"
       v-model:value="toothVal"
-      :rangeLimit="rangeLimit"
-      :disabled="rangeLimit === 'fullMouth' || rangeLimit === 'bitewing'"
+      :rangeLimit="props.rangeLimit"
+      :disabled="props.rangeLimit === 'fullMouth' || props.rangeLimit === 'bitewing'"
       @change="toothValChange"
     />
-    <div class="footer" v-if="multipleToothList?.length">
+    <div class="footer" v-if="props.multipleToothList && props.multipleToothList?.length">
       <div class="label">多生牙：</div>
       <div class="value">
         <CheckBox v-model:value="toothVal" @change="toothValChange" mutex :mutexOptionValue="['-1']">
-          <template v-for="ele in multipleToothList" :key="ele.value">
+          <template v-for="ele in props.multipleToothList" :key="ele.value">
             <CheckBoxItem
-              :disabled="rangeLimit === 'fullMouth'"
+              :disabled="props.rangeLimit === 'fullMouth'"
               :id="ele.value"
               :value="ele.value"
               :label="ele.label"
@@ -44,12 +44,7 @@ import CheckBoxItem from '../../CheckBox/src/item.vue'
 // 象限牙位
 import Tooth from './tooth.vue'
 import { nextTick, onUnmounted, ref, watch } from 'vue'
-const {
-  value = [],
-  multipleToothList = [],
-  rangeLimit,
-  name
-} = defineProps<{
+const props = defineProps<{
   value: string[]
   multipleToothList?: {
     label: string
@@ -58,12 +53,14 @@ const {
   name?: string
   rangeLimit?: 'fullMouth' | 'tooth' | 'bitewing' | string
 }>()
-const toothVal = ref(value)
+
+const toothVal = ref(props.value)
 
 watch(
-  () => value,
-  () => {
-    toothVal.value = value
+  () => props.value,
+  (val) => {
+    console.log(val)
+    toothVal.value = val
   },
   { deep: true }
 )
@@ -81,6 +78,7 @@ const toothValChange = () => {
       // 如果数组最后一个元素是'-1'，则将数组设置为只包含'-1'
       if (toothVal.value[toothVal.value.length - 1] == '-1') {
         // 触发更新事件和变化事件
+        toothVal.value = ['-1']
         emit('update:value', toothVal.value)
         emit('change', toothVal.value)
       } else {
@@ -91,6 +89,7 @@ const toothValChange = () => {
         emit('change', toothVal.value)
       }
     } else {
+      toothVal.value = []
       // 如果数组为空，触发更新事件和变化事件
       emit('update:value', [])
       emit('change', [])
@@ -108,28 +107,34 @@ onUnmounted(() => {
 .odos-tooth-posit {
   width: 489px;
   height: auto;
+
   * {
     box-sizing: border-box;
   }
+
   .header {
     height: 32px;
     display: flex;
     align-items: center;
     justify-content: space-between;
+
     .complete {
       :deep .odos-check-box-item {
         background: #ffffff;
         border: 0.5px solid #c9cdd4;
         border-radius: 6px;
+
         &.active {
           background: #2e6ce4;
           color: #ffffff;
         }
       }
     }
+
     .info {
       display: flex;
       align-items: center;
+
       span {
         margin-left: 4px;
         color: #2e6ce4;
@@ -137,14 +142,17 @@ onUnmounted(() => {
       }
     }
   }
+
   .footer {
     display: flex;
     align-items: center;
     margin-top: 14px;
+
     .label {
       font-size: 14px;
       color: #4e5969;
     }
+
     :deep .odos-check-box {
       .odos-check-box-item {
         border-radius: 4px;
@@ -156,6 +164,7 @@ onUnmounted(() => {
         align-items: center;
         background: #ffffff;
         border: 0.5px solid #c9cdd4;
+
         &.active {
           background: #2e6ce4;
           color: #ffffff;
