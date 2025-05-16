c0b1c712d0e9c7c8a1b869e42cd631160d0cb062
kwr011024 <kwr011024@163.com>
2025-05-16 10:16:51 +0800

    新增插槽示例组件并更新文档，优化Input组件API表格格式，添加主题切换功能至App组件。

---
+++ packages/Input/docs/slots.vue
@@ -0,0 +1,12 @@
+<template>
+  <odos-input>
+    <template #prefix>
+      <p>prefix</p>
+    </template>
+  </odos-input>
+  <odos-input>
+    <template #suffix>
+      <p>suffix</p>
+    </template>
+  </odos-input>
+</template>
