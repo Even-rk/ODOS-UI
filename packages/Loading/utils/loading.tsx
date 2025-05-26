923bf7b533203659a86164fa1c021d3066b1445d
kwr011024 <kwr011024@163.com>
2025-05-26 15:29:48 +0800

    Revert "优化Loading组件，移除LoadingView组件及相关逻辑，简化代码结构。"
    This reverts commit f3d21cddd7937174984ff4a0668f69d754e60b8c.

--- packages/Loading/utils/loading.tsx
+++ packages/Loading/utils/loading.tsx
@@ -1,5 +1,5 @@
 // 全局loading
-import { defineComponent, onMounted, ref } from 'vue'
+import { createApp, defineComponent, onMounted, onUnmounted, ref } from 'vue'
 import './loading.scss'
 
 // 组件
@@ -52,7 +52,40 @@ export const Loading = defineComponent({
     )
   }
 })
+const LoadingView = defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const index = ref(0)
+    const tips = props.text.split('\n').filter((tip) => tip.trim())
+    let timer: number
 
+    onMounted(() => {
+      // 设置初始值
+      index.value = Math.floor(Math.random() * tips.length)
+      timer = window.setInterval(() => {
+        index.value = Math.floor(Math.random() * tips.length)
+      }, 5000)
+    })
+
+    onUnmounted(() => {
+      if (timer) {
+        clearInterval(timer)
+      }
+    })
+
+    return () => (
+      <div class="odos-loading">
+        <Loading />
+        <div class="odos-loading-text">{tips[index.value]}</div>
+      </div>
+    )
+  }
+})
 export const useLoadingHide = () => {
   const target = document.querySelectorAll('.odos-modal-loading')
   if (target) {
@@ -63,10 +96,20 @@ export const useLoadingHide = () => {
 }
 
 export const useLoadingShow = async (time?: number) => {
+  const tipUrl = 'https://orange-odos.oss-cn-hangzhou.aliyuncs.com/assets/file/loading_tips.txt'
+  const response = await fetch(tipUrl, {
+      headers: {
+        'Cache-Control': 'no-cache'
+      }
+    }
+  )
+  const text = await response.text()
   // 创建
+  const loader = createApp(LoadingView, { text })
   const mountNode = document.createElement('div')
   mountNode.className = 'odos-modal-loading'
   document.body.appendChild(mountNode)
+  loader.mount(mountNode)
   if (time) {
     setTimeout(() => {
       useLoadingHide()
