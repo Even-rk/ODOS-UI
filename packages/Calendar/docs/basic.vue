ad4139570053772c28f814382bc349027859347f
kwr011024 <kwr011024@163.com>
2025-08-26 17:36:34 +0800

    refactor(Calendar): 更新组件导入方式并添加全局类型声明
    将Calendar组件从局部导入改为全局组件使用
    在全局组件类型声明文件中添加OdosCalendar类型

--- packages/Calendar/docs/basic.vue
+++ packages/Calendar/docs/basic.vue
@@ -1,12 +1,9 @@
 <template>
-  <div>
-    <Calendar v-model:value="date" />
-  </div>
+  <OdosCalendar v-model:value="date" />
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import Calendar from '../src/index.vue'
 
 const date = ref('2024-07-15')
 </script>
